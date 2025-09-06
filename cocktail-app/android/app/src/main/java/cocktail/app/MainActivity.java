c2b83e593362753d16c1444cdacc70e5b8550fa8
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-06 11:58:02 +0200

    Setup: Setup vue + ionic cocktail-app

---
+++ cocktail-app/android/app/src/main/java/cocktail/app/MainActivity.java
@@ -0,0 +1,5 @@
+package cocktail.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ cocktail-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cocktail.app',
+  appName: 'cocktail-app',
+  webDir: 'dist'
+};
+
+export default config;
