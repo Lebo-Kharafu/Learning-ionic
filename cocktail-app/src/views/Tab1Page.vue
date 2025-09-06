b4b275154e678ff2e22155aa2e324099fe87079c
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-06 17:36:11 +0200

    feat: Completed Random Page

--- cocktail-app/src/views/Tab1Page.vue
+++ cocktail-app/src/views/Tab1Page.vue
@@ -5,19 +5,104 @@
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content v-if="state.loading">
+      <section class="loading-center">
+        <ion-spinner color="primary"></ion-spinner>
+      </section>
+    </ion-content>
+    <ion-content :fullscreen="true" v-else>
+      <ion-refresher slot="fixed" @ionRefresh="doRefresh">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+      <ion-card>
+        <!-- @vue-ignore -->
+        <img :src="state.randomCocktail.strDrinkThumb"></img>
+        <ion-card-header>
+          <ion-card-subtitle>
+            <!-- @vue-ignore -->
+            {{ state.randomCocktail.strCategory }} | Served In
+            {{ state.randomCocktail.strGlass }}
+          </ion-card-subtitle>
+          <ion-card-title>
+            <!-- @vue-ignore -->
+            {{ state.randomCocktail.strDrink }}
+          </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          <p>
+            <!-- @vue-ignore -->
+            {{ state.randomCocktail.strInstructions }}
+          </p>
+          <ion-list-header>
+            Ingredients
+          </ion-list-header>
+          <ion-list>
+            <template v-for="indx in 15" :key="indx">
+              <!-- @vue-ignore -->
+              <ion-item v-if="state.randomCocktail[`strIngredient${indx}`]">
+                <ion-label>
+                  <!-- @vue-ignore -->
+                  <span v-if="state.randomCocktail[`strMeasure${indx}`]">
+                    <!-- @vue-ignore -->
+                    {{ state.randomCocktail[`strMeasure${indx}`] }} -
+                  </span>
+                  <!-- @vue-ignore -->
+                  {{ state.randomCocktail[`strIngredient${indx}`] }}
+                </ion-label>
+              </ion-item>
+            </template>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
 
-      <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { reactive } from 'vue';
+import axios from 'axios';
+
+const state = reactive({
+  randomCocktail: {},
+  loading: false
+});
+
+const fetchRandomCocktail = async (displayLoader: boolean) => {
+  if (displayLoader) {
+    state.loading = true;
+  }
+
+  const res = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+
+  if (res.data) {
+    state.randomCocktail = res.data?.drinks[0];
+  }
+
+  state.loading = false;
+  console.log(res);
+}
+
+const doRefresh = (event: CustomEvent) => {
+  fetchRandomCocktail(false);
+  //@ts-ignore
+  event.target?.complete();
+}
+
+fetchRandomCocktail(true);
+
 </script>
+
+<style lang="css">
+.loading-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90dvh;
+}
+
+ion-spinner {
+  transform: scale(1.5);
+}
+</style>
